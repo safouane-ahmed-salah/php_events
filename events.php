ba6ab21db907fd5a1ff7324d9c67d7053b7696bd
safwan39 <53286721+safwan39@users.noreply.github.com>
2021-03-23 15:14:41 +0800

    Create events.php

---
+++ events.php
@@ -0,0 +1,17 @@
+<php
+
+function _on($action, $fn){
+	global $__ACTIONS;
+	if(is_callable($fn)) $__ACTIONS[$action][] = $fn;
+}
+
+function _trigger($action, ...$args){
+	global $__ACTIONS;
+	$ret = $args[0] ?? null;
+	if(!empty($__ACTIONS[$action])){  
+		foreach($__ACTIONS[$action] as $fn) 
+			$ret = call_user_func_array($fn, $args);
+	}
+	
+	return $ret;
+}
